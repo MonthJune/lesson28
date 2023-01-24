937fac6939e71605dcf7be99ba67bde980499c9d
Оксана <needorogo@gmail.com>
2023-01-24 19:47:23 +0200

    Initial commit

---
+++ src/js/my.js
@@ -0,0 +1,46 @@
+/*function findUniq(arr) {
+    let res = [];
+    for (let i of arr) {
+        let str = (arr.filter(a => a !== i)).map(i => i.split(' ').join('')).join('').toLowerCase();
+        for (let j of i) {
+            if (str.includes(j.toLowerCase()) || j === ' ') {
+                continue;
+            } else {
+                res.push(j);
+            }
+        }
+    }
+    return arr.filter(i => {
+        for (let k of res) {
+            if (i.includes(k)) return i;
+        }
+    }).join('');
+  }*/
+//findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]) === 'BbBb'
+//findUniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]) === 'foo'
+
+
+let a = ['Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a'];
+let b = [ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ];
+let c = ['silvia', 'vasili', 'victor'];
+let d = ['Tom Marvolo Riddle', 'I am Lord Voldemort', 'Harry Potter'];
+
+function findUniq(arr) {
+    let str = arr.join('').toLowerCase();
+    let map = new Map();
+for (let i = 0; i < str.length; i++ ) {
+    if (map.has(str[i])) {
+        map.set(str[i], map.get(str[i]) + 1);
+    } else {
+     map.set(str[i], 0);
+    }
+}
+    return map;
+}
+
+console.log(findUniq(a));
+console.log(findUniq(b));
+console.log(findUniq(c));
+console.log(findUniq(d));
+
+
