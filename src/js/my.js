effc878fe7f4f7e210adecb6fe5bad0b2a5f3a7b
easy <chmega130@gmail.com>
2023-01-26 12:08:21 +0200

    last variant

--- src/js/my.js
+++ src/js/my.js
@@ -1,23 +1,4 @@
-/*function findUniq(arr) {
-    let res = [];
-    for (let i of arr) {
-        let str = (arr.filter(a => a !== i)).map(i => i.split(' ').join('')).join('').toLowerCase();
-        for (let j of i) {
-            if (str.includes(j.toLowerCase()) || j === ' ') {
-                continue;
-            } else {
-                res.push(j);
-            }
-        }
-    }
-    return arr.filter(i => {
-        for (let k of res) {
-            if (i.includes(k)) return i;
-        }
-    }).join('');
-  }*/
-//findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]) === 'BbBb'
-//findUniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]) === 'foo'
+
 
 
 let a = ['Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a'];
@@ -36,22 +17,14 @@ for (let i = 0; i < str.length; i++ ) {
      map.set(str[i], 0);
     }
 }
-//console.log(map);
-/*for (let i of map) {
-    if (i[1] === 0) {
-        unicLettter = (i[0]);
-        break;
-    }    
-}*/
-        
+
     for (let i of map) {
         if ((arr.filter(j => j.toLowerCase().includes(i[0]))).length === 1) {
         unicLettter = (i[0]);
         break;
         }
     }
-    //console.log((arr.filter(j => j.toLowerCase().includes('b'))).length === 1);
-    //console.log(unicLettter);
+
     return arr.find(i => { if ((i.toLowerCase().includes(unicLettter))) return i;});
 }
 
